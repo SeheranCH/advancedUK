0146c5d31891b1fa54f1a76b315fa5435cbe8f93
Ivo Gurtner <44357050+seheranch@users.noreply.github.com>
2020-04-09 13:18:48 +0200

    Add sum functional interface

--- UK/src/main/java/ch/course223/advanced/UK/UkApplication.java
+++ UK/src/main/java/ch/course223/advanced/UK/UkApplication.java
@@ -2,8 +2,9 @@ package ch.course223.advanced.UK;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UkApplication {
 
 	public static void main(String[] args) {
