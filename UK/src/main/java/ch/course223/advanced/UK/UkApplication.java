fa980e2a64fa18f3fb4d780d431264c979eddb88
Ivo Gurtner <ivo.gurtner@edu.tbz.ch>
2020-04-09 10:06:56 +0200

    Add new Spring boot application

---
+++ UK/src/main/java/ch/course223/advanced/UK/UkApplication.java
@@ -0,0 +1,13 @@
+package ch.course223.advanced.UK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkApplication.class, args);
+	}
+
+}
---
+++ UK/src/test/java/ch/course223/advanced/UK/UkApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.course223.advanced.UK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
